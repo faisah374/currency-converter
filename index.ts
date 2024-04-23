6a2f3f36b08e174c3d2dd4038fbbf33eefd7317e
Faisal Hameed <faisalh374@gmail.com>
2024-04-23 16:40:33 +0500

    second

---
+++ index.ts
@@ -0,0 +1,42 @@
+import inquirer from "inquirer"
+const currency:any ={
+    USD :1,
+    EUR: 0.91,
+    INR:83.25,
+    PKR:278,
+    SUDR:74,
+    UAER:76
+};
+let user_answer =await inquirer.prompt(
+    [
+    {
+name:"from",
+message:"Enter from currency",
+type:"list",
+choices:['USD','EUR','PKR','INR','SUDR','UAER']
+
+},
+{
+    name:'to',
+    message:'Enter to currency',
+    type: "list",
+ choices:[ 'USD','EUR','PKR','INR',"SUDR",'UAER']
+},
+  {
+    name:'amount',
+    messag:"enter amount",
+    type: "number",    
+    }
+
+  
+
+
+
+    ]
+);
+let fromamount = currency[user_answer.from]
+let toamount=currency [user_answer.to]
+let amount= user_answer.amount
+let baseAmount= amount/ fromamount //usd base currency
+let converterAmount =baseAmount * toamount
+console.log(converterAmount);
